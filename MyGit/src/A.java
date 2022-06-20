1bbfd7fc9743da8bb14fdc655f940891e70f193b
USER-PC <USER-PC@DESKTOP-CJMG9H0>
2022-06-20 22:49:17 +0900

    change devB

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,7 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is A");
-		System.out.println("this is A2");
+		System.out.println("this is changed A");
 
 	}
 
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -3,5 +3,6 @@ public class B {
 
 	public void sayB() {
 		System.out.println("this is B");
+		System.out.println("this is changed B");
 	}
 }
